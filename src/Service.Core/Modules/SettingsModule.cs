16f428a5a479c7400d1d7bdf07a5558330aa0ec2
dmitriy.b <dmitriy.b@smplt.net>
2021-12-14 09:39:23 +0500

    Main project added

---
+++ src/Service.Core/Modules/SettingsModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Service.Core.Modules
+{
+    public class SettingsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterInstance(Program.Settings).AsSelf().SingleInstance();
+        }
+    }
+}
