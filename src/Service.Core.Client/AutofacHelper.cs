2979c95221525dcb68cbb4c39aae583c0ce6ae2f
dmitriy.b <dmitriy.b@smplt.net>
2021-12-14 09:49:29 +0500

    Client project added

---
+++ src/Service.Core.Client/AutofacHelper.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+// ReSharper disable UnusedMember.Global
+
+namespace Service.Core.Client
+{
+	public static class AutofacHelper
+	{
+		public static void RegisterCoreClient(this ContainerBuilder builder, string grpcServiceUrl)
+		{
+		}
+	}
+}
---
+++ src/Service.Core.Client/CoreClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using MyJetWallet.Sdk.Grpc;
+
+namespace Service.Core.Client
+{
+	[UsedImplicitly]
+	public class CoreClientFactory : MyGrpcClientFactory
+	{
+		public CoreClientFactory(string grpcServiceUrl) : base(grpcServiceUrl)
+		{
+		}
+	}
+}
