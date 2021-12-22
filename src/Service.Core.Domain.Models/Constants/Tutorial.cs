6ae1965e7ac5482a0c8d588b5100d5b9c43909c9
dmitriy.b <dmitriy.b@smplt.net>
2021-12-22 19:51:17 +0500

    Add tutorial enum

---
+++ src/Service.Core.Domain.Models/Constants/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿namespace Service.Core.Domain.Models.Constants
+{
+	public enum Tutorial
+	{
+		None = 0,
+		PersonalFinance = 1,
+		BehavioralFinance = 2,
+		FinancialServices = 3,
+		FinanceMarkets = 4,
+		HealthAndFinance = 5,
+		PsychologyAndFinance = 6,
+		FinanceSecurity = 7,
+		TimeManagement = 8,
+		Economics = 9
+	}
+}
