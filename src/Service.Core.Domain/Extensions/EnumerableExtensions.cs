16fd9754ea44c02a6ebee7b4bb180fc9bf695b79
dmitriy.b <dmitriy.b@smplt.net>
2021-12-23 18:18:31 +0500

    Add extension

--- src/Service.Core.Domain/Extensions/EnumerableExtensions.cs
+++ src/Service.Core.Domain/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Service.Core.Domain.Extensions
@@ -6,5 +7,9 @@ namespace Service.Core.Domain.Extensions
 	public static class EnumerableExtensions
 	{
 		public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) => enumerable == null || !enumerable.Any();
+
+		public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> enumerable, bool predicate, Func<T, bool> expression) => predicate
+			? enumerable.Where(expression)
+			: enumerable;
 	}
 }
