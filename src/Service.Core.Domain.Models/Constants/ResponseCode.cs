7fb201a3f183cd4ba305360e0fe3fb9fa82405d8
dmitriy.b <dmitriy.b@smplt.net>
2022-01-14 10:05:32 +0500

    Add std response codes and objects

---
+++ src/Service.Core.Domain.Models/Constants/ResponseCode.cs
@@ -0,0 +1,15 @@
+﻿namespace Service.Core.Domain.Models.Constants
+{
+	public class ResponseCode
+	{
+		public const int Ok = 0;
+
+		public const int Fail = -1;
+
+		public const int UserNotFound = -2;
+
+		public const int NoRequestData = -3;
+
+		public const int NoResponseData = -4;
+	}
+}
