eccfb5c85ca6854df0349e17a45d13ea182ff6d7
dmitriy.b <dmitriy.b@smplt.net>
2022-01-19 13:26:49 +0500

    Add user reward types

---
+++ src/Service.Core.Domain.Models/Constants/UserStatus.cs
@@ -0,0 +1,78 @@
+﻿namespace Service.Core.Domain.Models.Constants
+{
+	/// <summary>
+	///     Статусы
+	/// </summary>
+	public enum UserStatus
+	{
+		/// <summary>
+		///     Мастер открытости
+		/// </summary>
+		MasterOfOpenness,
+
+		/// <summary>
+		///     Новичок
+		/// </summary>
+		Newbie,
+
+		/// <summary>
+		///     2-курсник
+		/// </summary>
+		SecondYearStudent,
+
+		/// <summary>
+		///     Бакалавр
+		/// </summary>
+		Bachelor,
+
+		/// <summary>
+		///     Магистр
+		/// </summary>
+		Magister,
+
+		/// <summary>
+		///     Аналитик
+		/// </summary>
+		Analyst,
+
+		/// <summary>
+		///     Стратег
+		/// </summary>
+		Strategist,
+
+		/// <summary>
+		///     Финансист
+		/// </summary>
+		Financier,
+
+		/// <summary>
+		///     Инвестор
+		/// </summary>
+		Investor,
+
+		/// <summary>
+		///     Лучший друг
+		/// </summary>
+		BestFriend,
+
+		/// <summary>
+		///     Долгожитель
+		/// </summary>
+		LongLiver,
+
+		/// <summary>
+		///     Знаток
+		/// </summary>
+		Expert,
+
+		/// <summary>
+		///     Награжденный
+		/// </summary>
+		Rewarded,
+
+		/// <summary>
+		///     Мастер ключа
+		/// </summary>
+		KeyMaster
+	}
+}
