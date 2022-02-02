a9b3714eb33a8e17d74a2ee75ee8ab0976320e1d
dmitriy.b <dmitriy.b@smplt.net>
2022-02-02 11:28:31 +0500

    Interface fixed

--- src/Service.Core.Client/Services/IEncoderDecoder.cs
+++ src/Service.Core.Client/Services/IEncoderDecoder.cs
@@ -3,8 +3,12 @@
 	public interface IEncoderDecoder
 	{
 		string Encode(string str);
+		string EncodeBytes(byte[] data);
+		string EncodeProto(object obj);
 
 		string Decode(string str);
+		byte[] DecodeBytes(string str);
+		T DecodeProto<T>(string str) where T : class;
 
 		string Hash(string str);
 	}
